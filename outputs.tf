14d645451c6c24c8672a753ba16a26080e4aa878
Damien Duportal <damien.duportal@gmail.com>
2025-04-11 12:28:02 +0200

    cleanup: finish cleanup for ci.jenkins.io and VPC
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,5 @@
 resource "local_file" "jenkins_infra_data_report" {
   content = jsonencode({
-    "${local.ci_jenkins_io["controller_vm_fqdn"]}" = {
-      "outbound_ips" = {
-        "agents" = module.vpc.nat_public_ips,
-      },
-      "ec2-agents" = {
-        "subnet_ids" = [module.vpc.private_subnets[0]],
-      },
-    }
   })
   filename = "${path.module}/jenkins-infra-data-reports/aws-sponsorship.json"
 }
--- variables.tf
+++ variables.tf
@@ -9,12 +9,6 @@ variable "aws_profile" {
   description = "AWS CLI profile to use with terraform"
 }
 
-variable "outbound_ip_count" {
-  type        = number
-  description = "Number of outbound IPs to use."
-  default     = 2
-}
-
 variable "terratest" {
   type        = bool
   description = "value"
