b228de0a2fbdb25e3ec0b4b3ccb5881f60a8f8a0
Jenkins Infra Bot (updatecli) <60776566+jenkins-infra-bot@users.noreply.github.com>
2025-06-20 14:52:56 +0000

    chore: Update the Terraform "vpc-endpoints" sub-module version of ter...
    ... raform-aws-sponsored in vpc.tf
    
    Made with ❤️️ by updatecli

--- vpc.tf
+++ vpc.tf
@@ -48,7 +48,7 @@ module "vpc" {
 ################################################################################
 module "vpc_endpoints" {
   source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-  version = "5.21.0"
+  version = "6.0.0"
 
   vpc_id = module.vpc.vpc_id
 
