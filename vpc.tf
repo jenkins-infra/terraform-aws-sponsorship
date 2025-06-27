18e3f532bca5ed15ad7eb3f93446f2118b49cab5
Damien Duportal <damien.duportal@gmail.com>
2025-06-27 07:28:42 +0200

    Revert "Bump version of the AWS Terraform module "vpc" to 6.0.1"

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@
 ####################################################################################
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "6.0.1"
+  version = "6.0.0"
 
   name = "aws-sponso-vpc"
   cidr = local.vpc_cidr
@@ -48,7 +48,7 @@ module "vpc" {
 ################################################################################
 module "vpc_endpoints" {
   source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-  version = "6.0.1"
+  version = "6.0.0"
 
   vpc_id = module.vpc.vpc_id
 
