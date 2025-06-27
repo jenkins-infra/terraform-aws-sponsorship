f22fd9d41ba4feb279e9e528f8dded3c7f49a987
Damien Duportal <damien.duportal@gmail.com>
2025-06-27 07:13:06 +0200

    Revert "Merge pull request #212 from dduportal/cleanup/ci.jenkins.io-agents-2/phase-2"
    This reverts commit 4b118568f322b202f35910102ab69ea6c2e00cd3, reversing
    changes made to 8a42204e31a20b338323e0cc40647ef0b42b0c21.

--- versions.tf
+++ versions.tf
@@ -14,6 +14,26 @@ terraform {
     cloudinit = {
       source = "hashicorp/cloudinit"
     }
+    # Required by the EKS module
+    null = {
+      source = "hashicorp/null"
+    }
+    # Required by the EKS module
+    time = {
+      source = "hashicorp/time"
+    }
+    # Required by the EKS module
+    tls = {
+      source = "hashicorp/tls"
+    }
+    # Required by the EKS module
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    # Required by the EKS module
+    helm = {
+      source = "hashicorp/helm"
+    }
     # Required by the secrets-manager module
     random = {
       source = "hashicorp/random"
