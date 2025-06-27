29cf72f56024ea2f69c58436ce3457c67231d99e
Damien Duportal <damien.duportal@gmail.com>
2025-06-27 08:01:58 +0200

    cleanup: remove all resources as a prequisite to provider downgrade
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- ecr.tf
+++ ecr.tf
@@ -1,65 +1,65 @@
-data "aws_secretsmanager_secret" "dockerhub_readonly" {
-  name = "ecr-pullthroughcache/dockerhub"
-}
+# data "aws_secretsmanager_secret" "dockerhub_readonly" {
+#   name = "ecr-pullthroughcache/dockerhub"
+# }
 
-module "ecr" {
-  source  = "terraform-aws-modules/ecr/aws"
-  version = "2.4.0"
+# module "ecr" {
+#   source  = "terraform-aws-modules/ecr/aws"
+#   version = "2.4.0"
 
-  create_repository = false
+#   create_repository = false
 
-  registry_pull_through_cache_rules = {
-    ecr = {
-      ecr_repository_prefix = "ecr"
-      upstream_registry_url = "public.ecr.aws"
-    }
-    k8s = {
-      ecr_repository_prefix = "k8s"
-      upstream_registry_url = "registry.k8s.io"
-    }
-    quay = {
-      ecr_repository_prefix = "quay"
-      upstream_registry_url = "quay.io"
-    }
-    dockerhub = {
-      ecr_repository_prefix = "docker-hub"
-      upstream_registry_url = "registry-1.docker.io"
-      credential_arn        = data.aws_secretsmanager_secret.dockerhub_readonly.arn
-    }
-  }
+#   registry_pull_through_cache_rules = {
+#     ecr = {
+#       ecr_repository_prefix = "ecr"
+#       upstream_registry_url = "public.ecr.aws"
+#     }
+#     k8s = {
+#       ecr_repository_prefix = "k8s"
+#       upstream_registry_url = "registry.k8s.io"
+#     }
+#     quay = {
+#       ecr_repository_prefix = "quay"
+#       upstream_registry_url = "quay.io"
+#     }
+#     dockerhub = {
+#       ecr_repository_prefix = "docker-hub"
+#       upstream_registry_url = "registry-1.docker.io"
+#       credential_arn        = data.aws_secretsmanager_secret.dockerhub_readonly.arn
+#     }
+#   }
 
-  manage_registry_scanning_configuration = true
-  registry_scan_type                     = "ENHANCED"
-  registry_scan_rules = [
-    {
-      scan_frequency = "SCAN_ON_PUSH"
-      filter = [
-        {
-          filter      = "*"
-          filter_type = "WILDCARD"
-        },
-      ]
-    }
-  ]
-}
+#   manage_registry_scanning_configuration = true
+#   registry_scan_type                     = "ENHANCED"
+#   registry_scan_rules = [
+#     {
+#       scan_frequency = "SCAN_ON_PUSH"
+#       filter = [
+#         {
+#           filter      = "*"
+#           filter_type = "WILDCARD"
+#         },
+#       ]
+#     }
+#   ]
+# }
 
-resource "aws_iam_policy" "ecrpullthroughcache" {
-  name = "ECRPullThroughCache"
+# resource "aws_iam_policy" "ecrpullthroughcache" {
+#   name = "ECRPullThroughCache"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ecr:CreateRepository",
-          "ecr:BatchImportUpstreamImage",
-          "ecr:TagResource"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = [
+#           "ecr:CreateRepository",
+#           "ecr:BatchImportUpstreamImage",
+#           "ecr:TagResource"
+#         ]
+#         Effect   = "Allow"
+#         Resource = "*"
+#       },
+#     ]
+#   })
 
-  tags = local.common_tags
-}
+#   tags = local.common_tags
+# }
--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,33 @@
-resource "local_file" "jenkins_infra_data_report" {
-  content = jsonencode({
-    "${local.ci_jenkins_io["controller_vm_fqdn"]}" = {
-      "name_servers" = aws_route53_zone.aws_ci_jenkins_io.name_servers,
-      "service_ips" = {
-        "ipv4" = aws_eip.ci_jenkins_io.public_ip,
-        "ipv6" = aws_instance.ci_jenkins_io.ipv6_addresses[0],
-      },
-      "outbound_ips" = {
-        "agents" = module.vpc.nat_public_ips,
-        "controller" = concat(
-          aws_instance.ci_jenkins_io.ipv6_addresses, # Public IPv6(s) (usually list of one element)
-          [aws_eip.ci_jenkins_io.public_ip],         # Public IPv4 of the controller
-        ),
-      },
-      "ec2-agents" = {
-        "subnet_ids" = [module.vpc.private_subnets[0]],
-        "security_group_names" = [
-          aws_security_group.ephemeral_vm_agents.name,
-          aws_security_group.unrestricted_out_http.name,
-        ]
-      },
-      artifacts_manager = {
-        s3_bucket_name = aws_s3_bucket.ci_jenkins_io_artifacts.bucket
-      },
-    },
+# resource "local_file" "jenkins_infra_data_report" {
+#   content = jsonencode({
+#     "${local.ci_jenkins_io["controller_vm_fqdn"]}" = {
+#       "name_servers" = aws_route53_zone.aws_ci_jenkins_io.name_servers,
+#       "service_ips" = {
+#         "ipv4" = aws_eip.ci_jenkins_io.public_ip,
+#         "ipv6" = aws_instance.ci_jenkins_io.ipv6_addresses[0],
+#       },
+#       "outbound_ips" = {
+#         "agents" = module.vpc.nat_public_ips,
+#         "controller" = concat(
+#           aws_instance.ci_jenkins_io.ipv6_addresses, # Public IPv6(s) (usually list of one element)
+#           [aws_eip.ci_jenkins_io.public_ip],         # Public IPv4 of the controller
+#         ),
+#       },
+#       "ec2-agents" = {
+#         "subnet_ids" = [module.vpc.private_subnets[0]],
+#         "security_group_names" = [
+#           aws_security_group.ephemeral_vm_agents.name,
+#           aws_security_group.unrestricted_out_http.name,
+#         ]
+#       },
+#       artifacts_manager = {
+#         s3_bucket_name = aws_s3_bucket.ci_jenkins_io_artifacts.bucket
+#       },
+#     },
 
-  })
-  filename = "${path.module}/jenkins-infra-data-reports/aws-sponsorship.json"
-}
-output "jenkins_infra_data_report" {
-  value = local_file.jenkins_infra_data_report.content
-}
+#   })
+#   filename = "${path.module}/jenkins-infra-data-reports/aws-sponsorship.json"
+# }
+# output "jenkins_infra_data_report" {
+#   value = local_file.jenkins_infra_data_report.content
+# }
