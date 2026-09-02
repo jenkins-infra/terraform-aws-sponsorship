64f8fae6e074cdea10614d695e5952fe0be50f25
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2026-09-02 20:01:44 +0200

    allow to pass an addon name or 'ami_release' as optional second parameter

--- get-addon-versions.sh
+++ get-addon-versions.sh
@@ -1,51 +1,74 @@
 #!/usr/bin/env bash
 set -eux -o pipefail
 
-# This script retrieves addons and AMI version corresponding to a Kubernetes version.
+# This script retrieves addons and AMI release version corresponding to a Kubernetes version.
+# If an addon name is passed as (optional) second parameter, it returns its corresponding version only.
+# If "ami_release" is passed as (optional) second parameter, it returns the corresponding AMI release version only.
 
 AWS_REGION="${AWS_REGION:-us-east-2}"
 AWS_PROFILE="${AWS_PROFILE:-jenkins-infra-admin}"
-ADDONS="${ADDONS:-coredns kube-proxy vpc-cni aws-ebs-csi-driver aws-mountpoint-s3-csi-driver eks-pod-identity-agent}"
+ADDON_LIST="${ADDON_LIST:-coredns kube-proxy vpc-cni aws-ebs-csi-driver aws-mountpoint-s3-csi-driver eks-pod-identity-agent}"
+AMI_TYPE="${AMI_TYPE:-amazon-linux-2023/arm64/standard/recommended}"
 
-if [[ "$#" -ne 1 ]]; then
-  echo "Usage: $0 <kubernetes-version>"
-  echo "Example: $0 1.34"
+if [[ "$#" -eq 0 ]]; then
+  echo "Usage: $0 <kubernetes-version> <addon-name>"
+  echo 'Examples:'
+  echo "$0 1.34"
+  echo "$0 1.34 coredns"
+  echo "$0 1.34 ami_release"
   exit 1
 fi
 
 k8s_version="$1"
-
-echo "Using Kubernetes version: ${k8s_version}"
-echo
+addon_name=''
+ami_release_only=false
+if [[ "$#" -eq 2 ]]; then
+    if [[ "$2" == 'ami_release' ]]; then
+        ami_release_only=true
+    elif [[ " ${ADDON_LIST} " == *" $2 "* ]]; then
+        addon_name="$2"
+    else
+        echo "Error: the parameter $2 is not recognized as addon or as 'ami_release'"
+        exit 1
+    fi
+fi
 
 get_addon_version() {
   addon_name="$1"
-
   aws eks describe-addon-versions \
     --kubernetes-version "${k8s_version}" \
     --region "${AWS_REGION}" \
     --profile "${AWS_PROFILE}" \
     --addon-name "${addon_name}" \
     --query 'addons[0].addonVersions[0].addonVersion' \
+	--no-cli-pager \
     --output text
 }
 
-echo "Fetching addon versions..."
-for addon in ${ADDONS}; do
-  version=$(get_addon_version "${addon}")
-  printf "%-35s %s\n" "${addon}:" "${version}"
-done
-
-echo "Fetching EKS optimized AMI release version..."
-ami_release_version=$(
+get_ami_release_version() {
   aws ssm get-parameters-by-path \
-    --path "/aws/service/eks/optimized-ami/${k8s_version}/" \
-    --recursive \
-    --query "Parameters[?contains(Name, 'amazon-linux-2023/arm64/standard/recommended')].Value" \
-    --region "${AWS_REGION}" \
-    --profile "${AWS_PROFILE}" \
-    --output json |
-  jq -r '.[0] | fromjson | .release_version'
-)
+	--path "/aws/service/eks/optimized-ami/${k8s_version}/" \
+	--recursive \
+	--query "Parameters[?contains(Name, '${AMI_TYPE}')].Value" \
+	--region "${AWS_REGION}" \
+	--profile "${AWS_PROFILE}" \
+	--output json \
+	| jq -r '.[0] | fromjson | .release_version'
+}
 
-echo "AMI release version: ${ami_release_version}"
+if [[ -n "${addon_name}" ]]; then
+    get_addon_version "${addon_name}"
+elif [[ "${ami_release_only}" == true ]]; then
+	get_ami_release_version
+else
+	echo "Fetching addon versions..."
+	for addon in ${ADDON_LIST}; do
+		version=$(get_addon_version "${addon}")
+		printf "%-35s %s\n" "${addon}:" "${version}"
+	done
+
+	echo "Fetching EKS optimized AMI release version..."
+	ami_release_version=$(get_ami_release_version)
+
+	echo "AMI release version: ${ami_release_version}"
+fi
