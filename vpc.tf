4de8ad450666fbee6513a5174aeb5ac3e6301d1b
Jenkins Infra Bot (updatecli) <60776566+jenkins-infra-bot@users.noreply.github.com>
2026-09-21 07:51:37 +0000

    chore: Update the Terraform "vpc" module version of terraform-aws-spo...
    ... nsored in vpc.tf
    
    Made with ❤️️ by updatecli

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@
 ####################################################################################
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "6.7.2"
+  version = "6.7.3"
 
   name = "aws-sponso-vpc"
   cidr = local.vpc_cidr
